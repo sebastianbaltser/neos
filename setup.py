ab3e328ebc1ae9de72e0e628a236b2705f01d6e8
Sebastian Baltser <sebastian.baltser@gmail.com>
2021-11-07 19:55:26 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neos",
+    description="A commandline app for communicating with the NEOS Optimization Server API.",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Click"
+    ],
+    author="Sebastian Baltser",
+    author_email="sebastian.baltser@gmail.com",
+    python_requires=">=3",
+    entry_points={
+        "console_scripts": ["neos = neos.scripts.main:neos"]
+    }
+)
